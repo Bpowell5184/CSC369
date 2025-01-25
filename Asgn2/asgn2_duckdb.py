a0e1e5af086a742ce15f2a40a6221dd8f98d242b
legion5184 <96603312+legion5184@users.noreply.github.com>
2025-01-24 18:44:56 -0800

    DuckDB Finished

--- Asgn2/asgn2_duckdb.py
+++ Asgn2/asgn2_duckdb.py
@@ -1,7 +1,67 @@
 import duckdb
+import sys
+import re 
+import time
 
-file_path = 'processed_canvas.parquet'
+# hardcode start/end dates/times
+start_date = "2022-04-03"
+start_hour = "04"
+end_date = "2022-04-03"
+end_hour = "10"
 
-#duckdb.read_parquet('processed_canvas.parquet')
+# Convert to valid format
+startDateTime = f"{start_date} {start_hour}:00:00"
+endDateTime = f"{end_date} {end_hour}:00:00"
 
-duckdb.sql(f"SELECT * FROM '{file_path}'")
+# Print timeframe
+print(f"Timeframe: {startDateTime} to {endDateTime}")
+
+# Connect to duckdb
+dbConnect = duckdb.connect()
+
+# Start time
+start_timer = time.perf_counter_ns()
+
+# Query data that only is in bounds of start/end time
+filtered_query = f"""
+SELECT * 
+FROM read_parquet('processed_canvas.parquet')
+WHERE timestamp >= TIMESTAMP '{startDateTime}' AND timestamp <= TIMESTAMP '{endDateTime}'
+"""
+
+# Query for max color
+color_query = f"""
+SELECT pixel_color, COUNT(*) AS frequency
+FROM ({filtered_query}) AS filtered_data
+GROUP BY pixel_color
+ORDER BY frequency DESC
+LIMIT 1
+"""
+
+# Query for max coord
+pixel_query = f"""
+SELECT x, y, COUNT(*) AS frequency
+FROM ({filtered_query}) AS filtered_data
+GROUP BY x, y
+ORDER BY frequency DESC
+LIMIT 1
+"""
+
+# Turn to vars
+most_frequent_color = dbConnect.execute(color_query).fetchone()
+most_frequent_pixel = dbConnect.execute(pixel_query).fetchone()
+
+# Get results
+color, color_frequency = most_frequent_color
+x_pixel, y_pixel, pixel_frequency = most_frequent_pixel
+
+# End timer
+end_timer = time.perf_counter_ns()
+
+# Calculate elapsed time
+elapsed_time_ns = end_timer - start_timer
+elapsed_time_ms = elapsed_time_ns / 1_000_000
+
+print(f"Elapsed time (ms): {elapsed_time_ms}")
+print(f"Most Popular Color: {most_frequent_color} ({color_frequency} occurences)")
+print(f"Most Popular Pixel: {x_pixel},{y_pixel} ({pixel_frequency} occurences)")
--- Asgn2/test_results_week2.md
+++ Asgn2/test_results_week2.md
@@ -12,9 +12,24 @@ Elapsed time (ms): 6.450167
 Most Popular Color: #000000 (694 occurences)
 Most Popular Pixel: 860,766 (8 occurences)
 ## 6 Hour Timeframe
-Timeframe: 2022-04-03 04:00:00 to 2022-04-03 11:00:00
-Elapsed time (ms): 9.177417
-Most Popular Color: #000000 (1850 occurences)
+Timeframe: 2022-04-03 04:00:00 to 2022-04-03 10:00:00
+Elapsed time (ms): 8.291583
+Most Popular Color: #000000 (1644 occurences)
 Most Popular Pixel: 0,0 (6 occurences)
 
 # DuckDB
+## 1 Hour Timeframe
+Timeframe: 2022-04-01 13:00:00 to 2022-04-01 14:00:00
+Elapsed time (ms): 10.979625
+Most Popular Color: #000000' (136 occurences)
+Most Popular Pixel: 741,741 (2 occurences)
+## 3 Hour Timeframe
+Timeframe: 2022-04-02 11:00:00 to 2022-04-02 14:00:00
+Elapsed time (ms): 11.522542
+Most Popular Color: #000000 (694 occurences)
+Most Popular Pixel: 860,766 (8 occurences)
+## 6 Hour Timeframe
+Timeframe: 2022-04-03 04:00:00 to 2022-04-03 10:00:00
+Elapsed time (ms): 11.017584
+Most Popular Color: #000000 (1644 occurences)
+Most Popular Pixel: 0,0 (6 occurences)
